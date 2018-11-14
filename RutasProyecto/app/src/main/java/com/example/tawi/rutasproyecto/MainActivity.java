ac668a01642e8aaffdae73404645cf5bcb26f31e
dianajim <dianajimalon@gmail.com>
2018-11-14 15:58:05 -0600

    primera prueba maps

--- RutasProyecto/app/src/main/java/com/example/tawi/rutasproyecto/MainActivity.java
+++ RutasProyecto/app/src/main/java/com/example/tawi/rutasproyecto/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tawi.rutasproyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ingresar=(Button)findViewById(R.id.button2);
+        ingresar.setOnClickListener(new View.OnClickListener()
+                                    {
+
+
+                                        @Override
+                                        public void onClick(View v) {
+
+                                            Intent pag=new Intent(MainActivity.this, com.example.tawi.rutasproyecto.MapsActivity.class);
+                                            startActivity(pag);
+                                        }
+                                    }
+        );
     }
 }
