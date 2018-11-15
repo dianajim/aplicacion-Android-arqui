a45c3f2c37df5a9e275b600d6f7367f23beb1fa6
dianajim <dianajimalon@gmail.com>
2018-11-15 17:15:12 -0600

    arreglos en mapa

--- RutasProyecto/app/src/main/java/com/example/tawi/rutasproyecto/MapsActivity.java
+++ RutasProyecto/app/src/main/java/com/example/tawi/rutasproyecto/MapsActivity.java
@@ -1,27 +1,50 @@
 package com.example.tawi.rutasproyecto;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Connection;
 
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public double lat_pos_ini=14.587687;
+    public double long_pos_ini=-90.553147;
+    public double lat_pos_fin=14.587687;
+    public double long_pos_fin=-90.553147;
+    public double lat_pos_act=14.587687;
+    public double long_pos_act=-90.553147;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        int status= GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+        if(status== ConnectionResult.SUCCESS) {
+
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+        else{
+            Dialog dialog=GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
+            dialog.show();
+        }
     }
 
 
@@ -37,10 +60,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        UiSettings uiSettings=mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        //
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(lat_pos_ini, long_pos_ini );
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Facultad de Ingenieria").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        float zoomlevel=16;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomlevel));
     }
 }
