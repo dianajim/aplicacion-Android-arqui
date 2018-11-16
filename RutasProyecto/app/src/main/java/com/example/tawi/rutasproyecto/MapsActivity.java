ea603fcbc751f0f2265402bb2bab75d819772289
dianajim <dianajimalon@gmail.com>
2018-11-15 21:11:34 -0600

    actualizacion de mapa con ruta de inicio, fin y mi ubicacion

--- RutasProyecto/app/src/main/java/com/example/tawi/rutasproyecto/MapsActivity.java
+++ RutasProyecto/app/src/main/java/com/example/tawi/rutasproyecto/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.tawi.rutasproyecto;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,16 +19,20 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    public double lat_pos_ini=14.587687;
-    public double long_pos_ini=-90.553147;
-    public double lat_pos_fin=14.587687;
-    public double long_pos_fin=-90.553147;
-    public double lat_pos_act=14.587687;
-    public double long_pos_act=-90.553147;
+    public double lat_pos_ini=14.587687; //latitud de Posicion de inicio de viaje
+    public double long_pos_ini=-90.553147;//longitud de Posicion de Inicio de viaje
+
+    public double lat_pos_fin=14.652525; //latitud de Posicion de fin  de Viaje
+    public double long_pos_fin=-90.488321;//longitud de posicion de fin de Viaje
+
+    public double lat_pos_act=14.626597; //latitud de Posicion actual de viaje
+    public double long_pos_act=-90.555731;//longitud de posicion actual de Viaje
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +65,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        UiSettings uiSettings=mMap.getUiSettings();
-        uiSettings.setZoomControlsEnabled(true);
-        //
 
-        LatLng sydney = new LatLng(lat_pos_ini, long_pos_ini );
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Facultad de Ingenieria").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-        float zoomlevel=16;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,zoomlevel));
+        //mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE); //Sirve para definir el tipo de mapa
+        UiSettings uiSettings=mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true); //permite activar el zoom del mapa
+        float zoomlevel=10;  //zoom del mapa
+
+
+        //marca de inicio
+        LatLng posicion_inicio = new LatLng(lat_pos_ini, long_pos_ini );
+        mMap.addMarker(new MarkerOptions().position(posicion_inicio).title("Inicio de Viaje").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posicion_inicio,zoomlevel));
+       //marca  de fin
+        LatLng fin_viaje = new LatLng(lat_pos_fin, long_pos_fin );
+        mMap.addMarker(new MarkerOptions().position(fin_viaje).title("Fin viaje").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fin_viaje,zoomlevel));
+        //marca de mi ubicacion
+        LatLng mi_posicion = new LatLng(lat_pos_act, long_pos_act );
+        mMap.addMarker(new MarkerOptions().position(mi_posicion).title("Mi Ubicacion").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mi_posicion,zoomlevel));
+
+        //Poligono de Ruta
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(lat_pos_ini, long_pos_ini), new LatLng(lat_pos_fin, long_pos_fin))
+                .width(5)
+                .color(Color.GREEN));
+
     }
 }
+
+
